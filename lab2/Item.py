c611ad6b5bac2a9552f28c0f8a54ea79fcff5a16
burur14 <90176858+burur14@users.noreply.github.com>
2022-04-06 00:39:28 +0300

    lab2

---
+++ lab2/Item.py
@@ -0,0 +1,7 @@
+class Item():
+    def __init__(self, name, releaseDate, expiryDate, price, lifeDuration):
+        self.name = name
+        self.releaseDate = releaseDate
+        self.expiryDate = expiryDate
+        self.price = price
+        self.lifeDuration = lifeDuration
